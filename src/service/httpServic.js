814cc5be1d4e44d03ff7abc2ca4b6ec3263c238f
pavithrant@nseit.com <pavithrant@nseit.com>
2023-07-18 19:49:43 +0530

    https service

--- src/service/httpServic.js
+++ src/service/httpServic.js
@@ -1,4 +1,8 @@
+//TMDB
 const API_KEY = 'api_key=5f04fdbed2deb3a7fe41a080a07a0e90';
 const BASE_URL = 'https://api.themoviedb.org/3';
 export const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 export const IMG_URL='https://image.tmdb.org/t/p/w500'
+
+// const API_KEY = "http://www.omdbapi.com/?i=tt3896198&apikey=ed2d3d17"
+// export const API_URL = API_KEY
